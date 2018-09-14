8d246e66041ecd80e3dd2fd26341a6f2936aab6a
Colby Stratten <cstratte@indiana.edu>
2018-09-14 17:18:23 -0400

    js application

---
+++ Public/js/userInfo.app.js
@@ -0,0 +1,84 @@
+
+var userInfo = new Vue({
+  el: '#userInfo',
+  data: {
+    result:
+    {
+      "gender": "",
+      "name": {
+        "title": "",
+        "first": "",
+        "last": ""
+      },
+      "location": {
+        "street": "",
+        "city": "",
+        "state": "",
+        "postcode": "",
+        "coordinates": {
+          "latitude": "",
+          "longitude": ""
+        },
+        "timezone": {
+          "offset": "",
+          "description": ""
+        }
+      },
+      "email": "",
+      "login": {
+        "uuid": "",
+        "username": "",
+        "password": "",
+        "salt": "",
+        "md5": "",
+        "sha1": "",
+        "sha256": ""
+      },
+      "dob": {
+        "date": "",
+        "age": 0
+      },
+      "registered": {
+        "date": "",
+        "age": 0
+      },
+      "phone": "",
+      "cell": "",
+      "id": {
+        "name": "",
+        "value": ""
+      },
+      "picture": {
+        "large": "",
+        "medium": "",
+        "thumbnail": ""
+      },
+      "nat": ""
+    },
+  "info": {
+    "seed": "",
+    "results": 1,
+    "page": 1,
+    "version": "1.2"
+  }
+  },
+  computed: {
+    calculated_age: function () {
+      return moment(this.result.dob).diff(moment(), 'days')
+    }
+  },
+  methods: {
+    fetchProject () {
+  fetch('https://randomuser.me/api/')
+  .then( response => response.json() )
+  .then( json => {personalInfo.result = json.results[0]} )
+  .catch( err => {
+    console.log('PROJECT FETCH ERROR:');
+    console.log(err);
+  })
+}
+  },
+  created () {
+      this.fetchProject();
+    }
+})
