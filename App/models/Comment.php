132e7279c7d711e9fb67e1c6816805b7f6d74295
Colby Stratten <cstratte@indiana.edu>
2018-10-09 15:06:12 -0400

    test

--- App/models/Comment.php
+++ App/models/Comment.php
@@ -31,7 +31,7 @@ class Comment
       // 4.a. For each row, make a new work object
       $comment =  new Comment($row);
 
-      //echo json_encode($comment);
+      echo json_encode($comment);
       array_push($commentarr, $comment);
     }
     return $commentarr;
